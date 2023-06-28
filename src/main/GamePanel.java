8e37869af0a8bceb7fe0f17916073d310704a004
Moshe <mosheyaku@gmail.com>
2023-06-29 00:23:33 +0300

    Setting the rectangle movements.

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,13 +1,40 @@
 package main;
 
+import inputs.KeyboardInputs;
+import inputs.MouseInputs;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class GamePanel extends JPanel {
-    public GamePanel(){
+    private MouseInputs mouseInputs;
 
+    private int xDelta=100, yDelta=100;
+    public GamePanel(){
+        mouseInputs= new MouseInputs(this);
+        addKeyListener(new KeyboardInputs(this));
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
+    }
+    public void changeXDelta(int value){
+        this.xDelta+=value;
+        repaint();
+    }
+
+    public void changeYDelta(int value){
+        this.yDelta+=value;
+        repaint();
+    }
+
+    public void setRectPosition(int x, int y){
+        this.xDelta=x;
+        this.yDelta=y;
+        repaint();
     }
     public void paintComponent(Graphics graphics){
         super.paintComponent(graphics);
+        graphics.fillRect(xDelta,yDelta,200,50);
     }
 }
