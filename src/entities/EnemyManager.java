b2c77bf53b2947d19d046d06cbd1677710bcd243
Moshe Yaku <mosheyaku@gmail.com>
2023-09-23 21:43:19 +0300

    Adding EnemyManager class.

---
+++ src/entities/EnemyManager.java
@@ -0,0 +1,45 @@
+package entities;
+
+import gamestates.Playing;
+import main.Game;
+import utils.LoadSave;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+import static utils.Constants.EnemyConstants.*;
+
+public class EnemyManager {
+    private Playing playing;
+    private BufferedImage[][] crabbyArr;
+    private ArrayList<Crabby> crabbies = new ArrayList<Crabby>();
+
+    public EnemyManager(Playing playing) {
+        this.playing = playing;
+        LoafEnemyImgs();
+    }
+
+    private void LoafEnemyImgs() {
+        crabbyArr = new BufferedImage[5][9];
+        BufferedImage temp = LoadSave.getPositionsAtlas(LoadSave.CRABBY_ATLAS);
+        for (int i = 0; i < crabbyArr.length; i++)
+            for (int j = 0; j < crabbyArr[i].length; j++)
+                crabbyArr[i][j] = temp.getSubimage(j * CRABBY_WIDTH_DEFAULT, i * CRABBY_HEIGHT_DEFAULT, CRABBY_WIDTH_DEFAULT, CRABBY_HEIGHT_DEFAULT);
+    }
+
+    public void update() {
+        for (Crabby c : crabbies)
+            c.update();
+    }
+
+    public void draw(Graphics graphics) {
+        drawCrabs(graphics);
+    }
+
+    private void drawCrabs(Graphics graphics) {
+        for (Crabby c : crabbies)
+            graphics.drawImage(crabbyArr[c.getEnemyState()][c.getAnimationIndex()], (int) c.getHitbox().x, (int) c.getHitbox().y, CRABBY_WIDTH, CRABBY_HEIGHT, null);
+    }
+
+}
